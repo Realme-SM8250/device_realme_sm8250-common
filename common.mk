87e23bcdcfbe4b2e617fbfd44683a237bc29acde
bibindasb <bibindasappu1@gmail.com>
2023-08-30 11:43:15 +0530

    sm8250-common: Build android.hardware.health@2.1-impl-qti

--- common.mk
+++ common.mk
@@ -279,7 +279,8 @@ PRODUCT_COPY_FILES += \
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
+    android.hardware.health@2.1-service \
+    android.hardware.health@2.1-impl-qti 
 
 # HIDL
 PRODUCT_PACKAGES += \
