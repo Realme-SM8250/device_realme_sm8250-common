a3e772fd0426f084ca47fbd8e22cee4d64e04332
bibindasb <bibindasappu1@gmail.com>
2023-08-30 11:31:13 +0530

    sm8250-common: Build missing GPS Libs

--- common.mk
+++ common.mk
@@ -256,7 +256,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gnss.measurement_corrections@1.1.vendor \
     android.hardware.gnss.visibility_control@1.0.vendor \
-    android.hardware.gnss@2.1.vendor
+    android.hardware.gnss@2.1.vendor \
+    android.hardware.gnss@2.1-impl-qti \
+    android.hardware.gnss@2.1-service-qti
+
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:$(TARGET_COPY_OUT_ODM)/etc/gps.conf
