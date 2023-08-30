f51675af1111165f8ecfac5eb875ebc4df3c8922
bibindasb <bibindasappu1@gmail.com>
2023-08-30 11:46:12 +0530

    sm8250-common:revert the unwanted power libs

--- common.mk
+++ common.mk
@@ -410,9 +410,6 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    android.hardware.power@1.2.vendor \
-    vendor.qti.hardware.perf@2.2.vendor \
     android.hardware.power-service.oplus-libperfmgr
 
 PRODUCT_COPY_FILES += \
