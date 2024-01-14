1f8ae80f2eb8ea76f5ab81867c0f9b5b905bf6e2
LuK1337 <priv.luk@gmail.com>
2024-01-14 20:12:46 +0530

    sm8250-common:Build and update dependency on libstdc++_vendor
    Fixes low mic gains in whatsapp and such apps.

--- common.mk
+++ common.mk
@@ -126,7 +126,12 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libcamera_metadata_shim \
-    vendor.qti.hardware.camera.postproc@1.0.vendor
+    libstdc++_vendor \
+    libc++_shared \
+    libcamera2ndk_vendor \
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    vendor.qti.hardware.camera.postproc@1.0.vendor \
+    android.frameworks.stats@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
